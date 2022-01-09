32cae98d393202d3b061e00963dab600e9701c60
meerfolk <meerfolk@gmail.com>
2022-01-09 07:35:34 +0500

    Add logger to web service

--- src/infrastructure/di-container/di.ts
+++ src/infrastructure/di-container/di.ts
@@ -1,3 +1,6 @@
+import { ILoggerService } from '../../domain/interfaces';
+
+import { ConsoleLoggerService } from '../logger';
 import { IWebService, FastifyWebService } from '../web';
 
 export class DIContainer {
@@ -7,15 +10,26 @@ export class DIContainer {
     constructor() {
         this.container = {
             WebService: this.WebServiceFactory(),
+            LoggerService: this.LoggerServiceFactory(),
         };
     }
 
     private WebServiceFactory(): IWebService {
         if (!this.singletones.has('WebService')) {
-            this.singletones.set('WebService', new FastifyWebService({ port: 3000 }));
+            const logger = this.LoggerServiceFactory();
+
+            this.singletones.set('WebService', new FastifyWebService({ port: 3000 }, logger));
         }
 
         return this.singletones.get('WebService') as typeof this.container['WebService'];
+    }
+
+    private LoggerServiceFactory(): ILoggerService {
+        if (!this.singletones.has('LoggerService')) {
+            this.singletones.set('LoggerService', new ConsoleLoggerService());
+        }
+
+        return this.singletones.get('LoggerService') as typeof this.container['LoggerService'];
     }
 
     public get<T extends keyof (typeof this.container)>(token: T): typeof this.container[T]{
--- src/infrastructure/web/fastify.service.ts
+++ src/infrastructure/web/fastify.service.ts
@@ -1,9 +1,10 @@
 import Fastify from 'fastify';
+import { ILoggerService } from '../../domain/interfaces';
 
 import { IWebService, IWebServiceOptions } from './interfaces';
 
 export class FastifyWebService implements IWebService {
-    constructor(private readonly options: IWebServiceOptions) {}
+    constructor(private readonly options: IWebServiceOptions, private readonly logger: ILoggerService) {}
 
     public async init(): Promise<void> {
         const server = Fastify({});
@@ -12,6 +13,8 @@ export class FastifyWebService implements IWebService {
             return 'OK';
         });
 
-        server.listen(this.options.port);
+        server.listen(this.options.port, () => {
+            this.logger.info(`Service started on ${this.options.port} port`);
+        });
     }
 }
