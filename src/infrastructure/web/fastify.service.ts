d2ee8a2f301e67ee08b02583eb934ef6cde63c34
meerfolk <meerfolk@gmail.com>
2022-01-13 00:36:19 +0500

    Implement web service interface from controller

--- src/infrastructure/web/fastify.service.ts
+++ src/infrastructure/web/fastify.service.ts
@@ -1,28 +1,30 @@
-import Fastify from 'fastify';
+import Fastify, { FastifyInstance } from 'fastify';
 
 import { ILoggerService } from '../../domain/interfaces';
-import { Controller } from '../../presentation/web/controller';
+import { IWebService, IRequest } from '../../presentation/web/controller';
 
-import { IWebService, IWebServiceOptions } from './interfaces';
+import { IWebServiceOptions } from './interfaces';
 
 export class FastifyWebService implements IWebService {
+    private readonly server: FastifyInstance;
+
     constructor(
-        private readonly controller: Controller,
         private readonly options: IWebServiceOptions,
         private readonly logger: ILoggerService,
-    ) {}
+    ) {
+        this.server = Fastify({});
+    }
 
-    public async init(): Promise<void> {
-        const server = Fastify({});
+    public addGetRoute<T>(path: string, handler: (req: IRequest) => Promise<T>): void {
+        this.server.get(path, async (req) => {
+            const result = await handler(req);
 
-        const routes = this.controller.getRoutes();
-
-        server.get(Controller.GET_DEFAULT_EP_SYMBOL.toString(), async () => {
-            const baseGetHandler = routes[Controller.GET_DEFAULT_EP_SYMBOL];
-            baseGetHandler();
+            return result;
         });
+    }
 
-        server.listen(this.options.port, () => {
+    public async start(): Promise<void> {
+        this.server.listen(this.options.port, () => {
             this.logger.info(`Service started on ${this.options.port} port`);
         });
     }
