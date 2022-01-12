a69cdcef4cbbfe3137c708feacc2364eca7d9e16
meerfolk <meerfolk@gmail.com>
2022-01-13 00:39:02 +0500

    Use controller to start web application

--- src/presentation/web/main.ts
+++ src/presentation/web/main.ts
@@ -1,8 +1,12 @@
 import { DIContainer } from '../../infrastructure/di-container';
 
+import { Controller } from './controller';
+
 const di = new DIContainer();
 const webService = di.get('WebService');
 
 (async function() {
-    await webService.init();
+    const controller = new Controller(webService);
+
+    controller.webServiceStart();
 })()
