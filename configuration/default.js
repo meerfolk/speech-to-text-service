1d5a6f53a1e987a7a97953d5cc0081a2fe93e40e
meerfolk <meerfolk@gmail.com>
2022-01-30 23:04:43 +0500

    Add yandex cloud configuration

--- configuration/default.js
+++ configuration/default.js
@@ -2,4 +2,18 @@ module.exports = {
     web: {
         port: process.env.PORT || 3000,
     },
+    cloud: {
+        yandex: {
+            storage: {
+                id: process.env.YANDEX_STORAGE_KEY,
+                key: process.env.YANDEX_STORAGE_SECRET,
+                bucket: process.env.YANDEX_STORAGE_BUCKET,
+                region: process.env.YANDEX_STORAGE_REGION,
+            },
+            speechkit: {
+                apiKey: process.env.YANDEX_SPEECHKIT_API_KEY,
+            },
+        },
+    }
+
 };
