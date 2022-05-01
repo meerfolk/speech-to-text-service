0328cc7db1fedbf00388d02cfd83746f510e0d4e
meerfolk <meerfolk@gmail.com>
2022-05-01 20:51:46 +0500

    Add yandex recognize service

---
+++ src/infrastructure/cloud/yandex/recognize.service.ts
@@ -0,0 +1,76 @@
+import { IHttpRequestService, RawResponse } from '~/domain/interfaces';
+import { UploadModel, SpeechRecognitionModel } from '~/domain/models';
+
+import { IYandexStorageOptions } from './interfaces';
+
+type YandexSpeechkitBodyType = {
+    config: {
+        specification: {
+            languageCode: string;
+        };
+    };
+    audio: {
+        uri: string;
+    };
+};
+
+export class YandexSpeechkitService {
+    private static SPEECHKIT_ENDPOINT = 'https://transcribe.api.cloud.yandex.net/speech/stt/v2/longRunningRecognize';
+    private static STORAGE_ENPOINT_TEMPLATE = 'https://storage.yandexcloud.net/<bucket>/<key>';
+
+    constructor(
+        private readonly httpRequestService: IHttpRequestService,
+        private readonly storageOptions: IYandexStorageOptions,
+        private readonly speechKitApiKey: string,
+    ) {}
+
+    private getFileUrl(fileName: string): string {
+        return YandexSpeechkitService.STORAGE_ENPOINT_TEMPLATE
+            .replace('<bucket>', this.storageOptions.bucket)
+            .replace('<key>', fileName);
+    }
+
+    private getServiceUrl(): string {
+        return YandexSpeechkitService.SPEECHKIT_ENDPOINT;
+    }
+
+    private generateBody(fileName: string): YandexSpeechkitBodyType {
+        return {
+            config: {
+                specification: {
+                    languageCode: 'ru-RU',
+                },
+            },
+            audio: {
+                uri: this.getFileUrl(fileName),
+            },
+        };
+    }
+
+    private generateHeaders(): Record<string, string> {
+        return {
+            'Authorization': `Api-Key ${this.speechKitApiKey}`,
+        };
+    }
+
+    private async sendRequest(model: UploadModel): Promise<[RawResponse, unknown]> {
+        const url = this.getServiceUrl();
+        const body = this.generateBody(model.name);
+        const headers = this.generateHeaders();
+
+        const response = await this.httpRequestService.postRaw(url, body, {
+            headers,
+        });
+
+        return response;
+    }
+
+    public async recognize(model: UploadModel): Promise<SpeechRecognitionModel | null> {
+        const [_res, body] = await this.sendRequest(model);
+
+        const { id } = body as { id: string };
+
+        return new SpeechRecognitionModel(id);
+    }
+}
+
--- src/infrastructure/cloud/yandex/yandex.service.ts
+++ src/infrastructure/cloud/yandex/yandex.service.ts
@@ -1,19 +1,39 @@
-import { ICloudService, UploadModel } from '~/domain';
+import {
+  ICloudService,
+  IHttpRequestService,
+  UploadModel,
+  SpeechRecognitionModel,
+} from '~/domain';
 
-import { YandexUploadService, IYandexStorageOptions } from './upload.service';
+import { YandexUploadService, } from './upload.service';
+import { YandexSpeechkitService } from './recognize.service';
+import { IYandexStorageOptions } from './interfaces';
 
 interface IYandexServiceOptions {
     storage: IYandexStorageOptions;
+    speechkit: {
+        apiKey: string;
+    };
 };
 
 export class YandexService implements ICloudService {
     private readonly uploadService: YandexUploadService;
+    private readonly recognitionService: YandexSpeechkitService;
 
-    constructor(options: IYandexServiceOptions) {
+    constructor(options: IYandexServiceOptions, httpService: IHttpRequestService) {
         this.uploadService = new YandexUploadService(options.storage);
+        this.recognitionService = new YandexSpeechkitService(
+          httpService,
+          options.storage,
+          options.speechkit.apiKey,
+        );
     }
 
     public async upload(model: UploadModel): Promise<void> {
         this.uploadService.upload(model);
+    }
+
+    public async recognize(model: UploadModel): Promise<SpeechRecognitionModel | null> {
+      return this.recognitionService.recognize(model);
     }
 }
