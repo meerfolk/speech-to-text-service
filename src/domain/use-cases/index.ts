5ed79ffbb74b76fa7dfabdb291130f78a2e022d4
meerfolk <meerfolk@gmail.com>
2022-09-09 21:26:53 +0500

    Add use case to pull new recognition

--- src/domain/use-cases/index.ts
+++ src/domain/use-cases/index.ts
@@ -1,3 +1,4 @@
 export * from './recognize-file.use-case';
 export * from './get-recognition.use-case';
 export * from './get-recognition-list.use-case';
+export * from './pull-new-recognitions.use-case';
